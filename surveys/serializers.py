917ae6f9a2f1a3a9a82b063ca25ab3903b90de91
BecaLHughes <s5126768@bournemouth.ac.uk>
2022-05-12 18:01:15 +0100

    Created serilaizers for displaying and accepting responses in a survey

--- surveys/serializers.py
+++ surveys/serializers.py
@@ -1,26 +1,44 @@
 # Django
 from rest_framework import serializers
 
+# Local
+from .models import Survey, SurveyQuestion
+
 # -------------------
-class SurveySerializer(serializers.Serializer):
-    title = serializers.CharField()
+class SurveyQuestionSerializer(serializers.ModelSerializer):
+    question = serializers.CharField(source='question.text')
 
-class SurveyQuestionSerializer(serializers.Serializer):
-    survey = serializers.CharField()
-    question = serializers.CharField()
-    order = serializers.IntegerField()
+    class Meta:
+        model = SurveyQuestion
+        fields = ['order', 'question']
 
-class QuestionSerializer(serializers.Serializer):
-    text = serializers.CharField()
+class SurveySerializer(serializers.ModelSerializer):
+    questions = serializers.SerializerMethodField()
 
-class FeedbackSerializer(serializers.Serializer):
-    text = serializers.CharField()
-    score_range = serializers.IntegerField()
+    class Meta: 
+        model = Survey
+        fields = ['id', 'title', 'questions']
 
-class ScoreSerializer(serializers.Serializer):
-    time_stamp = serializers.DateTimeField()
-    score = serializers.IntegerField()
-    feedback = serializers.CharField()
+    def get_questions(self, survey):
+        survey_questions = SurveyQuestion.objects.filter(survey=survey)
+        serializer = SurveyQuestionSerializer(survey_questions, many=True) 
+        return serializer.data
 
-    
+class SurveyResponseSerializer(serializers.Serializer):
+    question = serializers.IntegerField()
+    value = serializers.IntegerField()
 
+    def validate_question(self, question_id):
+        survey = self.context["survey"]
+        survey_question = SurveyQuestion.objects.filter(survey=survey, question__id=question_id).first()
+        if survey_question is None:
+            raise serializers.ValidationError({'question': f'Question with ID {question_id} does not exist for survey'})
+        
+        return question_id
+
+    def validate_value(self, value):
+        range = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        if (value in range) is False:
+            raise serializers.ValidationError({'value': 'Must be a valid range value'})
+        
+        return value
