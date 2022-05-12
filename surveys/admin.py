7fcd32dfbfd64d0c90c0d08226b3e0546acf71c3
BecaLHughes <s5126768@bournemouth.ac.uk>
2022-05-12 17:56:11 +0100

    Added list_display to admins classes, added survey question admin

--- surveys/admin.py
+++ surveys/admin.py
@@ -3,22 +3,26 @@ import imp
 from django.contrib import admin
 
 # Local
-from .models import Survey, Feedback, Question, Score
+from .models import Survey, SurveyQuestion, Question, Feedback, Score
 
 # --------------------------
 class SurveyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'title', )
 
-class FeedbackAdmin(admin.ModelAdmin):
-    pass
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'survey', 'question')
 
 class QuestionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'text', )
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', )
 
 class ScoreAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Survey, SurveyAdmin)
-admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(SurveyQuestion, SurveyQuestionAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(Score, ScoreAdmin)
