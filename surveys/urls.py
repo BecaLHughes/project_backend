3f12f424ba351dea66e2d6f721a3aeb14f3e39d1
BecaLHughes <s5126768@bournemouth.ac.uk>
2022-05-12 20:27:33 +0100

    update to survey id url with backslash

--- surveys/urls.py
+++ surveys/urls.py
@@ -6,6 +6,6 @@ from .views import SurveyDetailView, SurveyResponseView
 
 # -------------
 urlpatterns = [
-    path('<int:survey_id>', SurveyDetailView.as_view()),
+    path('<int:survey_id>/', SurveyDetailView.as_view()),
     path('<int:survey_id>/responses/', SurveyResponseView.as_view())
 ]
